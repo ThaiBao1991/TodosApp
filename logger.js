d3bd026dca577fa3c19bee99c201cc49f44d0a44
ThaiBao <ThaiBao1991dn@gmail.com>
2023-05-19 13:49:20 +0700

    upload todoapp

---
+++ logger.js
@@ -0,0 +1,14 @@
+export default function logger(reducer){
+    return (prevstate, action, args) => {
+
+        console.group(action)
+        console.log('Prev State', prevstate)
+        console.log('Action Arguments', args)
+
+        const nextState = reducer(prevstate, action, args)
+        console.log('Next State', nextState)
+
+        console.groupEnd()
+        return nextState
+    }
+}
